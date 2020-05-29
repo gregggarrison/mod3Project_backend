d57980eabff95007d91f0efd803698a7b9e4dca8
gregggarrison <gregg.garrison@rocketmail.com>
2020-05-29 13:14:46 -0600

    first commit

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,5 @@
 class Score < ApplicationRecord
+    validates :name, {
+        presence: true  
+    }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
 Score.destroy_all
 
-# score1 = Score.create(name: "Gregg", score: 100)
+score1 = Score.create(name: "Gregg", score: 322)
+score1 = Score.create(name: "Sally", score: 443)
+score1 = Score.create(name: "Pat", score: 111)
+score1 = Score.create(name: "Erica", score: 333)
+score1 = Score.create(name: "Clint", score: 240)
+score1 = Score.create(name: "Joe", score: 40)
+score1 = Score.create(name: "Kelly", score: 10)
+score1 = Score.create(name: "Mike", score: 110)
+score1 = Score.create(name: "Phil", score: 20)
+score1 = Score.create(name: "Tom", score: 222)
+
